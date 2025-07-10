3028dda9c3ba6a8e60c96031d198936f34c08085
Gajendra Kumar Arya <gajendra.work538@gmail.com>
2025-07-10 20:42:01 +0530

    added webSocket connection

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ const io = new Server(server, {
 });
 
 app.use(cookieParser());
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors({origin: "https://talksy-bq2qbn4m6-gajendraarya786s-projects.vercel.app/", credentials: true}));
 app.use(express.json({limit: "40kb"}))
 app.use(express.urlencoded({limit: "40kb"}));
 
