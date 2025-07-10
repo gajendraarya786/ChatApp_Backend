7e78f05220977bd270e03ebe02fcaab32e6371a8
Gajendra Kumar Arya <gajendra.work538@gmail.com>
2025-07-10 21:13:33 +0530

    added webSocket connection

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ dotenv.config();
 
 connectDb();
 
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 //Creating an http server
@@ -46,6 +47,6 @@ app.use("/api/v1/users", userRouter);
 
 socketHandler(io);
 
-server.listen(process.env.PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
